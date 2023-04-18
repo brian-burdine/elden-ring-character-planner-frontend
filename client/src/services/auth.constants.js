a0f5b9a7806ecd139367135bf5e08d4047cc5217
brian-burdine <brian.burdine@student.awesomeinc.org>
2023-04-18 17:37:38 +0000

    Set up front end to handle user authentication

---
+++ client/src/services/auth.constants.js
@@ -0,0 +1,4 @@
+export const API_URL = "https://8000-brianburdin-eldenringch-w9dw94jpxfw.ws-us94.gitpod.io/";
+export const LOGIN_ENDPOINT = "user/login/"
+export const REGISTER_ENDPOINT = "user/signup/"
+export const REFRESH_ENDPOINT = "token/refresh/"
