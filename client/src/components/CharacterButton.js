8d2e41d63970cdf384577ef1ad9d25e3a5b0a2d9
brian-burdine <brian.burdine@student.awesomeinc.org>
2023-04-25 15:37:32 +0000

    Imported starting class data, starting to use it

--- client/src/components/CharacterButton.js
+++ client/src/components/CharacterButton.js
@@ -11,7 +11,8 @@ function CharacterButton({ buttonType }) {
       ...state,
       currentCharacter: {
         id: "",
-        name: ""
+        name: "",
+        starting_class: 1
       }
     })
     navigate("/planner");
@@ -35,6 +36,7 @@ function CharacterButton({ buttonType }) {
             method: 'POST',
             data: {
               "name": state.currentCharacter.name,
+              "starting_class": state.currentCharacter.starting_class,
               "owner": state.currentUser.user_id
             },
           });
@@ -69,12 +71,16 @@ function CharacterButton({ buttonType }) {
 
   if (buttonType === "add") {
     return (
-      <button 
-        className="btn add-button"
-        onClick={handleAddACharacter}
-      >
-        +
-      </button>
+      <>
+        <button 
+          className="btn add-button"
+          id="add-character"
+          onClick={handleAddACharacter}
+        >
+          +
+        </button>
+        <label htmlFor="add-character">Add a character</label>
+      </>
     )
   }
   else if (buttonType === "save") {
--- client/src/components/planner/Planner.js
+++ client/src/components/planner/Planner.js
@@ -2,17 +2,24 @@ import { useEffect, useState } from "react";
 import { useGlobalState } from "../../context/GlobalState";
 import request from "../../services/api.request";
 import CharacterNameField from "./CharacterNameField";
+import StartingClassSelectField from "./StartingClassSelectField";
 import CharacterButton from "../CharacterButton";
 
 function Planner () {
     const [ state, dispatch ] = useGlobalState();
+    const [startingClasses, setStartingClasses] = useState([]);
 
-    // useEffect(() => {
-    //     async function getERData () {
-    //         let options = {}
-    //     }
-    //     getERData();
-    // }, [])
+    useEffect(() => {
+        async function getERData () {
+            let options = {
+                url: 'starting_classes/',
+                method: 'GET'
+            }
+            let response = await request(options);
+            setStartingClasses(response.data);
+        }
+        getERData();
+    }, [])
 
     return (
         <div className="container" id="planner-page">
@@ -21,6 +28,7 @@ function Planner () {
                 <div className="col-md">
                     <h3 className="column-header">Basic Characteristics</h3>
                     <CharacterNameField />
+                    <StartingClassSelectField />
                 </div>
                 <div className="col-md-5">
                     <h3 className="column-header">Equipment</h3>
