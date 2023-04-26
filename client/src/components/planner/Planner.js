28a15eb6ee9dd554768acc1503967265da44f1e7
brian-burdine <brian.burdine@student.awesomeinc.org>
2023-04-26 20:43:27 +0000

    Started layout of weapons

--- client/src/components/planner/Planner.js
+++ client/src/components/planner/Planner.js
@@ -75,6 +75,22 @@ function Planner () {
                 </div>
                 <div className="col-md-5">
                     <h3 className="column-header">Equipment</h3>
+                    <div className="weapons">
+                        <h5>Weapons</h5>
+                        <p>Right Hand</p>
+                        <ol>
+                            <li>Weapon!</li>
+                            <li>Weapon!</li>
+                            <li>Weapon!</li>
+                        </ol>
+                        <p>Left Hand</p>
+                        <ol>
+                            <li>Weapon!</li>
+                            <li>Weapon!</li>
+                            <li>Weapon!</li>
+                        </ol>
+                    </div>
+                    
                 </div>
                 <div className="col-md">
                     <h3 className="column-header">Derived Statistics</h3>
